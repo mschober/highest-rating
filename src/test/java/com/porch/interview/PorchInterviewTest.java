d5bd619c475107bec38ea13a1587f3e5b69f9ee8
Michael Schober <mschober@expedia.com>
2014-09-08 19:37:01 -0700

    using OpenCSV to read csv values

--- src/test/java/com/porch/interview/PorchInterviewTest.java
+++ src/test/java/com/porch/interview/PorchInterviewTest.java
@@ -1,17 +1,32 @@
 package com.porch.interview;
 
 
+import au.com.bytecode.opencsv.CSVReader;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
 public class PorchInterviewTest {
 
     @Test
-    public void testExample(){
+    public void testExample() throws IOException {
         assertEquals(1, 1);
+        URL file = this.getClass().getResource("/reviews/1");
+        CSVReader reader = new CSVReader(new FileReader(file.getFile()), '\t');
+        String[] header = reader.readNext();
+        List<String[]> values = reader.readAll();
+        System.out.println(Arrays.toString(header));
+        for(String[] line : values){
+            System.out.println(Arrays.toString(line));
+        }
+        assertEquals(1, values.size());
     }
 }
